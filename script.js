94e69c60e0cb406ec479b2cff5b53d5f38b17265
Yuriy Ulanov <1859697@gmail.com>
2023-09-20 15:36:53 +0300

    task complete

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const products = ['product_1', 'product_2', 'product_3', 'product_4'];
 const domProducts = document.querySelectorAll('.gallery__item');
 const mainImage = document.getElementById('main_img');
 
+const color = document.getElementById('color');
+
 for (let i = 0; i < products.length; i++) {
   domProducts[i].src = `./assets/img/product_${i + 1}.jpg`;
   domProducts[i].addEventListener('click', () => {
     mainImage.src = domProducts[i].src;
   });
 }
+
+color.addEventListener('change', (e) => {
+  console.log(e.target.value);
+  mainImage.src = `./assets/img/product_${e.target.value}.jpg`;
+});
